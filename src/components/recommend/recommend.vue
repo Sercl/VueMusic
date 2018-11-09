cfa99dd7e98570cc4c4f5ca51b6f3a752fc06154
Sercl <wzryouxiang@126.com>
2018-11-09 14:02:09 +0800

    添加loading和图片懒加载

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -6,7 +6,7 @@
           <slider>
             <div v-for="item in recommends" :key="item.id">
               <a :href="item.linkUrl">
-                <img @load="loadImage" :src="item.picUrl" alt="">
+                <img class="needsclick" @load="loadImage" :src="item.picUrl" alt="">
               </a>
             </div>
           </slider>
@@ -16,7 +16,7 @@
           <ul>
             <li v-for="item in discList" class="item">
               <div class="icon">
-                <img width="60" height="60" :src="item.imgurl" alt="">
+                <img width="60" height="60" v-lazy="item.imgurl" alt="">
               </div>
               <div class="text">
                 <h2 class="name" v-html="item.creator.name"></h2>
--- src/main.js
+++ src/main.js
@@ -3,10 +3,15 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastclick from 'fastclick'
-
+import VueLazyLoad from 'vue-lazyload'
 import 'common/stylus/index.styl'
 
 fastclick.attach(document.body)
+
+//图片懒加载
+Vue.use(VueLazyLoad, {
+  loading: require('common/image/default.png')
+})
 
 /* eslint-disable no-new */
 new Vue({
