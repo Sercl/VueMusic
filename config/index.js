be467ee07e36012740a4035ef6cb11ce6ae96c37
Sercl <wzryouxiang@126.com>
2018-12-29 03:10:30 +0800

    优化路由懒加载

--- config/index.js
+++ config/index.js
@@ -8,6 +8,7 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
+    //指定资源域名；CDN
     assetsPublicPath: '/',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
